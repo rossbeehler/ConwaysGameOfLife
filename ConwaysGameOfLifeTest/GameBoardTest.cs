e16ca45383868b62d0db81178d516274c0145089
Ross Beehler <ross@sipp.net>
2011-02-10 07:38:56 -0500

    Initial Checkin

---
+++ ConwaysGameOfLifeTest/GameBoardTest.cs
@@ -0,0 +1,203 @@
+﻿using ConwaysGame_StartAtBoard;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace TestProject1
+{
+    [TestClass()]
+    public class GameBoardTest
+    {
+        private static void AssertEqualBoard(GameBoard gameBoard, int[][] expectedState)
+        {
+            Assert.AreEqual(expectedState.Length, gameBoard.CurrentState.Length);
+
+            for (int boardRow = 0; boardRow < gameBoard.CurrentState.Length; boardRow++)
+            {
+                CollectionAssert.AreEqual(expectedState[boardRow], gameBoard.CurrentState[boardRow]);
+            }
+        }
+
+        [TestMethod]
+        public void TestEmptyBoard()
+        {
+            var g = new GameBoard(new int[][] 
+            {
+                new[] { 0, 0, 0 },
+                new[] { 0, 0, 0 },
+                new[] { 0, 0, 0 },
+            });
+
+            g.Tick();
+
+            AssertEqualBoard(g, new int[][] 
+            {
+                new[] { 0, 0, 0 },
+                new[] { 0, 0, 0 },
+                new[] { 0, 0, 0 },
+            });
+        }
+
+        [TestMethod]
+        public void TestOneAliveDies()
+        {
+            var g = new GameBoard(new int[][]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 1, 0 },
+                new [] { 0, 0, 0 },
+            });
+
+            g.Tick();
+
+            AssertEqualBoard(g, new int[][]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+            });
+        }
+
+        [TestMethod]
+        public void TestBlock()
+        {
+            var g = new GameBoard(new int[][]
+            {
+                new [] { 0, 0, 0, 0 },
+                new [] { 0, 1, 1, 0 },
+                new [] { 0, 1, 1, 0 },
+                new [] { 0, 0, 0, 0 },
+            });
+
+            g.Tick();
+
+            AssertEqualBoard(g, new int[][]
+            {
+                new [] { 0, 0, 0, 0 },
+                new [] { 0, 1, 1, 0 },
+                new [] { 0, 1, 1, 0 },
+                new [] { 0, 0, 0, 0 },
+            });            
+        }
+
+        [TestMethod]
+        public void TestLeftEdgeReturnsEmpty()
+        {
+            var g = new GameBoard(new int[][]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 1, 0, 0 },
+                new [] { 0, 0, 0 },
+            });
+
+            g.Tick();
+
+            AssertEqualBoard(g, new int[][]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+            });
+        }
+
+        [TestMethod]
+        public void TestRightEdgeReturnsEmpty()
+        {
+            var g = new GameBoard(new int[][]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 1 },
+                new [] { 0, 0, 0 },
+            });
+
+            g.Tick();
+
+            AssertEqualBoard(g, new int[][]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+            });
+        }
+
+        [TestMethod]
+        public void TestUpperEdgeReturnsEmpty()
+        {
+            var g = new GameBoard(new int[][]
+            {
+                new [] { 0, 1, 0 },
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+            });
+
+            g.Tick();
+
+            AssertEqualBoard(g, new int[][]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+            });
+        }
+
+        [TestMethod]
+        public void TestLowerEdgeReturnsEmpty()
+        {
+            var g = new GameBoard(new int[][]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+                new [] { 0, 1, 0 },
+            });
+
+            g.Tick();
+
+            AssertEqualBoard(g, new int[][]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+            });
+        }
+
+        [TestMethod]
+        public void TestFourCornersReturnEmpty()
+        {
+            var g = new GameBoard(new int[][]
+            {
+                new [] { 1, 0, 1 },
+                new [] { 0, 0, 0 },
+                new [] { 1, 0, 1 },
+            });
+
+            g.Tick();
+
+            AssertEqualBoard(g, new int[][]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+                new [] { 0, 0, 0 },
+            });
+        }
+
+        [TestMethod]
+        public void TestOneNeighborDies()
+        {
+            var g = new GameBoard(new int[][]
+            {
+                new [] { 0, 0, 0,},
+                new [] { 0, 0, 1,},
+                new [] { 0, 0, 1,},
+            });
+
+            g.Tick();
+
+            AssertEqualBoard(g, new int[][]
+            {
+                new [] { 0, 0, 0,},
+                new [] { 0, 0, 0,},
+                new [] { 0, 0, 0,},
+            });
+        }        
+    }
+}
---
+++ GameBoard.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConwaysGame_StartAtBoard
+{
+    public class GameBoard
+    {
+        private int[][] currentState;
+
+        public GameBoard(int[][] initialState)
+        {
+            this.currentState = initialState;
+        }
+
+        public void Tick()
+        {
+            for (int row = 0; row < currentState.Length; row++)
+            {
+                for (int column = 0; column < currentState[row].Length; column++)
+                {
+                    if (currentState[row][column] == 1)
+                    {
+                        if (AliveNeighbors(row, column) < 2)
+                        {
+                            currentState[row][column] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        public int[][] CurrentState
+        {
+            get
+            {
+                return this.currentState;
+            }
+        }
+
+        private int AliveNeighbors(int row, int column)
+        {
+            return Left(row, column) +
+                   Up(row, column) +
+                   UpperLeft(row, column) +
+                   UpperRight(row, column) +
+                   Right(row, column) +
+                   Down(row, column) +
+                   LowerRight(row, column) +
+                   LowerLeft(row, column);
+        }
+
+        private int LowerLeft(int row, int column)
+        {
+            if (IsOutsideBounds(column - 1, row + 1))
+                return 0;
+            return currentState[row + 1][column - 1];
+        }
+
+        private int LowerRight(int row, int column)
+        {
+            if (IsOutsideBounds(column + 1, row + 1))
+                return 0;
+            return currentState[row + 1][column + 1];
+        }
+
+        private int Down(int row, int column)
+        {
+            if (IsOutsideBounds(column, row + 1))
+                return 0;
+            return currentState[row + 1][column + 0];
+        }
+
+        private int Right(int row, int column)
+        {
+            if (IsOutsideBounds(column + 1, row))
+                return 0;
+            return currentState[row + 0][column + 1];
+        }
+
+        private int UpperRight(int row, int column)
+        {
+            if (IsOutsideBounds(column + 1, row - 1))
+                return 0;
+            return currentState[row - 1][column + 1];
+        }
+
+        private int UpperLeft(int row, int column)
+        {
+            if (IsOutsideBounds(column - 1, row - 1))
+                return 0;
+            return currentState[row - 1][column - 1];
+        }
+
+        private int Up(int row, int column)
+        {
+            if (IsOutsideBounds(column, row - 1))
+                return 0;
+            return currentState[row - 1][column - 0];
+        }
+
+        private int Left(int row, int column)
+        {
+            if (IsOutsideBounds(column - 1, row))
+                return 0;
+            return currentState[row - 0][column - 1];
+        }
+        
+        private bool IsOutsideBounds(int column, int row)
+        {
+            if (column < 0 || column >= currentState[0].Length ||
+                row < 0 || row >= currentState.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
